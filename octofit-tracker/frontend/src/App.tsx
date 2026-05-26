47088c549af01557b9159814c6bc04f3fbee8965
rogerwtrimble-tech <roger.w.trimble@gmail.com>
2026-05-26 02:20:30 +0000

    build front/backend

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Octofit Tracker</h1>
+      <p>Frontend running on port 5173</p>
+    </div>
+  )
+}
